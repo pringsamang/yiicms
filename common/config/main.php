ad698b9b68d6fe4bb805d8dbc3c3179eb5b674a4
msushil <np.msushil@gmail.com>
2014-12-31 16:20:37 +0545

    yii cms initial commit

---
+++ common/config/main.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ]
+        
+    ],
+];
